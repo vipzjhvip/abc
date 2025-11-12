a26bbd1e4f3b2f9ceb1869665d060b1764473841
vipzjhvip <124351637+vipzjhvip@users.noreply.github.com>
2025-11-12 19:08:31 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // 确保根目录正确
-  base: './', // 关键：部署时必须用相对路径
+  base: './', // ✅ 必须是相对路径
 })
