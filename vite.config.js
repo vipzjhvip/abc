7a0bdab3fea742cfceaa91396761e23ada0be286
vipzjhvip <124351637+vipzjhvip@users.noreply.github.com>
2025-11-12 19:05:50 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  root: '.', // 确保根目录正确
+  base: './', // 关键：部署时必须用相对路径
+})
